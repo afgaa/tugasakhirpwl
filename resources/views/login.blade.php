936e12e790c6b87c86a5b9bb255af86a052d4053
afgaa <primoafgaparlevi@gmail.com>
2022-06-15 12:13:09 +0700

    moo

--- resources/views/login.blade.php
+++ resources/views/login.blade.php
@@ -24,7 +24,7 @@
 
         <div class="col-lg-4 shadow p-5 bg-light" id="register">
             <main class="form-signin">
-                <h2 class="h3 mb-3 fw-normal text-center text-muted">Please <strong>Login Moo!!</strong></h2>
+                <h2 class="h3 mb-3 fw-normal text-center text-muted">Please <strong>Login</strong></h2>
 
                 <div class="text-center m-3"><i class="bi bi-person-circle" style="font-size: 100px;"></i></div>
                 <form action="/loginuser" method="POST">
