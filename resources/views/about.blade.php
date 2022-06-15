480c156702e7ccfe19966f7e2da1ca584a62603d
afgaa <primoafgaparlevi@gmail.com>
2022-06-15 12:36:50 +0700

    tampilan about

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -14,41 +14,21 @@
 <div class="container-fluid py-5">
     <div class="container">
         <div class="row align-items-center">
-            <div class="col-lg-7">
-                <h1 class="mb-4">SD</h1>
-                <p>Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos,
-                    ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est
-                    dolor</p>
-                <div class="row pt-2 pb-4">
-                    <div class="col-6 col-md-4">
-                        <img class="img-fluid rounded" src="{{asset ('assets/img/sd.png')}}" alt="">
-                    </div>
-                    <div class="col-6 col-md-8">
-                        <ul class="list-inline m-0">
-                            <li class="py-2 border-top border-bottom"><i class="fa fa-check text-primary mr-3"></i>Labore eos amet dolor amet diam</li>
-                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Etsea et sit dolor amet ipsum</li>
-                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Diam dolor diam elitripsum vero.</li>
-                        </ul>
-                    </div>
-                </div>
-                <a href="" class="btn btn-primary mt-2 py-2 px-4">Daftar</a>
-                <br><br>
-            </div>
 
             <div class="col-lg-7">
                 <h1 class="mb-4">SMP</h1>
-                <p>Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos,
-                    ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est
-                    dolor</p>
+                <p>SMP merupakan jenjang pendidikan dasar pada pendidikan formal di Indonesia yang 
+                    ditempuh setelah lulus sekolah dasar (atau sederajat). Sekolah menengah pertama 
+                    ditempuh dalam waktu 3 tahun, mulai dari kelas 7 sampai kelas 9.</p>
                 <div class="row pt-2 pb-4">
                     <div class="col-6 col-md-4">
                         <img class="img-fluid rounded" src="{{asset ('assets/img/smp.png')}}" alt="">
                     </div>
                     <div class="col-6 col-md-8">
                         <ul class="list-inline m-0">
-                            <li class="py-2 border-top border-bottom"><i class="fa fa-check text-primary mr-3"></i>Labore eos amet dolor amet diam</li>
-                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Etsea et sit dolor amet ipsum</li>
-                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Diam dolor diam elitripsum vero.</li>
+                            <li class="py-2 border-top border-bottom"><i class="fa fa-check text-primary mr-3"></i>Matematika</li>
+                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>IPA</li>
+                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Bahasa Indonesia</li>
                         </ul>
                     </div>
                 </div>
@@ -58,18 +38,18 @@
 
             <div class="col-lg-7">
                 <h1 class="mb-4">SMA</h1>
-                <p>Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos,
-                    ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est
-                    dolor</p>
+                <p>SMA merupakan  jenjang pendidikan menengah pada pendidikan formal di Indonesia setelah lulus 
+                    sekolah menengah pertama (atau sederajat). Sekolah menengah atas ditempuh dalam waktu 3 tahun, 
+                    mulai dari kelas 10 sampai kelas 12.</p>
                 <div class="row pt-2 pb-4">
                     <div class="col-6 col-md-4">
                         <img class="img-fluid rounded" src="{{asset ('assets/img/sma.jpg')}}" alt="">
                     </div>
                     <div class="col-6 col-md-8">
                         <ul class="list-inline m-0">
-                            <li class="py-2 border-top border-bottom"><i class="fa fa-check text-primary mr-3"></i>Labore eos amet dolor amet diam</li>
-                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Etsea et sit dolor amet ipsum</li>
-                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Diam dolor diam elitripsum vero.</li>
+                            <li class="py-2 border-top border-bottom"><i class="fa fa-check text-primary mr-3"></i>Bahasa Indonesia</li>
+                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Bahasa Inggris</li>
+                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>IPA</li>
                         </ul>
                     </div>
                 </div>
@@ -79,18 +59,17 @@
 
             <div class="col-lg-7">
                 <h1 class="mb-4">SMK</h1>
-                <p>Invidunt lorem justo sanctus clita. Erat lorem labore ea, justo dolor lorem ipsum ut sed eos,
-                    ipsum et dolor kasd sit ea justo. Erat justo sed sed diam. Ea et erat ut sed diam sea ipsum est
-                    dolor</p>
+                <p>SMK merupakan  pendidikan formal yang menyelenggarakan pendidikan kejuruan pada jenjang 
+                    pendidikan menengah setara dengan SMA/MA.</p>
                 <div class="row pt-2 pb-4">
                     <div class="col-6 col-md-4">
                         <img class="img-fluid rounded" src="{{asset ('assets/img/smk.png')}}" alt="">
                     </div>
                     <div class="col-6 col-md-8">
                         <ul class="list-inline m-0">
-                            <li class="py-2 border-top border-bottom"><i class="fa fa-check text-primary mr-3"></i>Labore eos amet dolor amet diam</li>
-                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Etsea et sit dolor amet ipsum</li>
-                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Diam dolor diam elitripsum vero.</li>
+                            <li class="py-2 border-top border-bottom"><i class="fa fa-check text-primary mr-3"></i>Bahasa Indonesia</li>
+                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Bahasa Inggris</li>
+                            <li class="py-2 border-bottom"><i class="fa fa-check text-primary mr-3"></i>Kejuruhan</li>
                         </ul>
                     </div>
                 </div>
--- resources/views/register-user.blade.php
+++ resources/views/register-user.blade.php
@@ -60,22 +60,4 @@
         margin-bottom: 5px;
     }
 </style>
-        <div class="row align-items-center px-3">
-            <div class="col-lg-6 text-center text-lg-left">
-                <h1 class="display-3 font-weight-bold text-white text-center">Tempat Belajar</h1>
-                <br><br>
-                <p class="text-white mb-4">Ddisini kami menyediakan layanan belajar untuk anak anak 
-                    SD, SMP dan SMA/SMK supaya mereka bisa melakukan pembelajaran dirumah tidak hanya di sekolahan.
-                </p>
-                <p>
-                    .
-                </p>
-                    <br><br><br>
-                    <br><br><br>
-            </div>
-            <div class="col-lg-6 text-center text-lg-right">
-                <img class="img-fluid mt-7" src="{{asset ('assets/img/jti.png')}}">
-            </div>
-        </div>
-    </div>
 @endsection
